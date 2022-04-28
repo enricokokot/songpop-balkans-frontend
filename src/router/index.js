211f85b51ed47e024f9effe9b8c43aabafc012f9
coco <kokotenrico@gmail.com>
2022-04-28 10:25:53 +0200

    Set up login page

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,20 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "home",
+    path: "/vuetify",
+    name: "vuetify",
     component: HomeView,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginView,
   },
   {
     path: "/about",
---
+++ src/views/LoginView.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-container fill-height fluid class="justify-center">
+    <div class="about d-flex flex-column">
+      <h1>This is a login page</h1>
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-text-field
+          v-model="email"
+          :rules="[rules.required]"
+          label="E-mail"
+          required
+        ></v-text-field>
+
+        <v-text-field
+          v-model="password"
+          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="[rules.required, rules.min]"
+          :type="show ? 'text' : 'password'"
+          label="Password"
+          hint="At least 8 characters"
+          counter
+          @click:append="show = !show"
+        ></v-text-field>
+
+        <!-- <v-select
+          v-model="select"
+          :items="genres"
+          :rules="[rules.required]"
+          label="Preferred Genre"
+          required
+        ></v-select> -->
+
+        <v-container class="d-flex flex-column">
+          <v-btn :disabled="!valid" color="success" @click="validate">
+            Validate
+          </v-btn>
+
+          <v-btn color="error" @click="reset"> Reset Form </v-btn>
+
+          <v-btn color="warning" @click="resetValidation">
+            Reset Validation
+          </v-btn>
+        </v-container>
+      </v-form>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    show: false,
+    valid: true,
+    email: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+    ],
+    password: "",
+    passwordRules: [
+      (v) => !!v || "Password is required",
+      (v) =>
+        (v && v.length >= 8) || "Password must be at least 8 characters long",
+    ],
+    select: null,
+    genres: ["Rock", "Pop", "Hip-Hop", "Metal"],
+    rules: {
+      required: (value) => !!value || "Required.",
+      min: (v) => v.length >= 8 || "Min 8 characters",
+      emailMatch: () => `The email and password you entered don't match`,
+    },
+  }),
+
+  methods: {
+    validate() {
+      this.$refs.form.validate();
+    },
+    reset() {
+      this.$refs.form.reset();
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    },
+  },
+};
+</script>
