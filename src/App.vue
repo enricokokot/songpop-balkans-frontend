6d6e94e787e2ba5c551fb6866320dd745acb4b19
coco <kokotenrico@gmail.com>
2022-05-04 16:33:57 +0200

    Add several routing paths

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <v-app>
-    <v-app-bar v-if="!$route.meta.hideNavbar" app color="primary" dark>
+    <v-app-bar v-if="!$route.meta.hideNavbar" app class="indigo" dark>
       <div class="d-flex align-center">
-        <v-img
+        <!-- <v-img
           alt="Vuetify Logo"
           class="shrink mr-2"
           contain
@@ -18,18 +18,20 @@
           min-width="100"
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
           width="100"
-        />
+        /> -->
+        <router-link
+          style="text-decoration: none; color: inherit"
+          to="/vuetify"
+        >
+          <h2>SONGPOP BALKANS</h2>
+        </router-link>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
+      <v-btn @click="goToLogin" text>
+        <span class="mr-2">Log out</span>
+        <v-icon>mdi-logout</v-icon>
       </v-btn>
     </v-app-bar>
 
@@ -46,5 +48,14 @@ export default {
   data: () => ({
     //
   }),
+
+  methods: {
+    goToHome() {
+      this.$router.push("/vuetify");
+    },
+    goToLogin() {
+      this.$router.push("/login");
+    },
+  },
 };
 </script>
