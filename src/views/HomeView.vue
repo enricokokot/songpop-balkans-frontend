5c6f2d5cfeb99cf6d8cd593aff96fa6ae0ff74a5
coco <kokotenrico@gmail.com>
2022-06-15 10:09:16 +0200

    Rename obscure variables

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,20 +2,20 @@
   <div class="about">
     <v-container class="text-justify">
       <h1>
-        This is {{ userInfo.name.split(" ")[0] }}'s ({{ userInfo.id }}) Home
+        This is {{ localUser.name.split(" ")[0] }}'s ({{ localUser.id }}) Home
         Page
       </h1>
-      <h2>Available playlists: {{ userInfo.playlists.join(", ") }}</h2>
-      <h2>Available coins: {{ userInfo.coins }}</h2>
-      <h2>Games played: {{ userInfo["games played"] }}</h2>
-      <h2>Games won: {{ userInfo["games won"] }}</h2>
-      <h2>Games lost: {{ userInfo["games lost"] }}</h2>
-      <h2>Games tied: {{ userInfo["games tied"] }}</h2>
+      <h2>Available playlists: {{ localUser.playlists.join(", ") }}</h2>
+      <h2>Available coins: {{ localUser.coins }}</h2>
+      <h2>Games played: {{ localUser["games played"] }}</h2>
+      <h2>Games won: {{ localUser["games won"] }}</h2>
+      <h2>Games lost: {{ localUser["games lost"] }}</h2>
+      <h2>Games tied: {{ localUser["games tied"] }}</h2>
       <h2>Duels waiting on your response:</h2>
       <li v-for="duel in duelsWaitingOnResponseFromYou" :key="duel.id">
         {{ duel.id }} against
         {{
-          allUsersWhoArentUserInfo.find((user) => user.id == duel.challengerId)
+          allUsersWhoArentLocalUser.find((user) => user.id == duel.challengerId)
             .name
         }}
         in
@@ -35,12 +35,12 @@
               <v-list-item-title
                 @click="
                   endADuel(
-                    userInfo.id,
+                    localUser.id,
                     allUsers.find((user) => user.id == duel.against).id,
                     item.title,
                     allUsers
                       .find((user) => user.id == duel.against)
-                      .duels.find((duel) => duel.against == userInfo.id).score
+                      .duels.find((duel) => duel.against == localUser.id).score
                   )
                 "
                 >{{ item.title }}</v-list-item-title
@@ -53,7 +53,7 @@
       <li v-for="duel in duelsWaitingOnResponseFromOthers" :key="duel.id">
         {{ duel.id }} against
         {{
-          allUsersWhoArentUserInfo.find(
+          allUsersWhoArentLocalUser.find(
             (user) => user.id == duel.challengeTakerId
           ).name
         }}
@@ -66,13 +66,13 @@
         <v-btn color="primary" @click="quitADuel(duel.id)">Quit</v-btn>
       </li>
       <!-- <h2>Active duels:</h2>
-      <li v-for="duel in userInfo.duels" :key="duel.id">
+      <li v-for="duel in localUser.duels" :key="duel.id">
         Against {{ allUsers.find((user) => user.id == duel.against).name }} in
         {{ duel.playlist.toUpperCase() }} with a result of: {{ duel.score }} -
         {{
           allUsers
             .find((user) => user.id == duel.against)
-            .duels.find((duel) => duel.against == userInfo.id).score
+            .duels.find((duel) => duel.against == localUser.id).score
         }} -->
       <!-- </li> -->
       <h2>Duel someone:</h2>
@@ -89,7 +89,7 @@
                 {{ user.name.split(" ")[0] }} in?</v-toolbar
               >
               <v-card-text>
-                <li v-for="playlist in userInfo.playlists" :key="playlist">
+                <li v-for="playlist in localUser.playlists" :key="playlist">
                   <v-menu top>
                     <template v-slot:activator="{ on, attrs }">
                       <v-btn color="primary" dark v-bind="attrs" v-on="on">
@@ -102,7 +102,7 @@
                         <v-list-item-title
                           @click="
                             sendADuel(
-                              userInfo.id,
+                              localUser.id,
                               user.id,
                               playlist,
                               item.title
@@ -130,9 +130,9 @@
 export default {
   name: "HomeView",
   data: () => ({
-    userInfo: {},
+    localUser: {},
     allUsers: [],
-    allUsersWhoArentUserInfo: [],
+    allUsersWhoArentLocalUser: [],
     duelsWaitingOnResponseFromYou: [],
     duelsWaitingOnResponseFromOthers: [],
     usersWhoAreYetToBeChallenged: [],
@@ -196,33 +196,32 @@ export default {
   async mounted() {
     const userResponse = await fetch("http://localhost:3000/user");
     const userData = await userResponse.json();
-    this.userInfo = userData[3];
+    this.localUser = userData[3];
     this.allUsers = userData;
-    // this.allUsersWhoArentUserInfo = this.allUsers.filter((user) => {
+    // this.allUsersWhoArentLocalUser = this.allUsers.filter((user) => {
     //   if (
-    //     user.id != this.userInfo.id &&
-    //     !this.userInfo.duels.find((duel) => duel.against == user.id)
+    //     user.id != this.localUser.id &&
+    //     !this.localUser.duels.find((duel) => duel.against == user.id)
     //   ) {
     //     return user;
     //   }
     // });
-    this.allUsersWhoArentUserInfo = this.allUsers.filter(
-      (user) => user.id != this.userInfo.id
+    this.allUsersWhoArentLocalUser = this.allUsers.filter(
+      (user) => user.id != this.localUser.id
     );
     const duelResponse = await fetch("http://localhost:3000/duel");
     const duelData = await duelResponse.json();
     this.duelsWaitingOnResponseFromYou = duelData.filter(
-      (duel) => duel.challengeTakerId == this.userInfo.id
+      (duel) => duel.challengeTakerId == this.localUser.id
     );
     this.duelsWaitingOnResponseFromOthers = duelData.filter(
-      (duel) => duel.challengerId == this.userInfo.id
+      (duel) => duel.challengerId == this.localUser.id
     );
     const playlistResponse = await fetch("http://localhost:3000/playlist");
     const playlistData = await playlistResponse.json();
     this.allPlaylists = playlistData;
-    const userIdsOfUsersWhoArentCurrentUser = this.allUsersWhoArentUserInfo.map(
-      (user) => user.id
-    );
+    const userIdsOfUsersWhoArentCurrentUser =
+      this.allUsersWhoArentLocalUser.map((user) => user.id);
     const userIdsOfChallengedUsers = this.duelsWaitingOnResponseFromYou.map(
       (duel) => duel.challengerId
     );
@@ -264,7 +263,7 @@ export default {
 
     this.usersWhoAreYetToBeChallenged = UserIdsOfUsersWhoAreYetToDuel.map(
       (userId) =>
-        this.allUsersWhoArentUserInfo.find((user) => user.id == userId)
+        this.allUsersWhoArentLocalUser.find((user) => user.id == userId)
     );
     // console.log(
     //   "this.usersWhoAreYetToBeChallenged",
