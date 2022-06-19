d7c2b4c3512efe6aed659e2abb9e1a5d46679f41
coco <kokotenrico@gmail.com>
2022-06-19 08:42:34 +0200

    Remove redundant components

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,12 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import VuetifyView from "../views/VuetifyView.vue";
 import LoginView from "../views/LoginView.vue";
 import HomeView from "../views/HomeView.vue";
+import TemplateView from "../views/TemplateView.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/vuetify",
-    name: "vuetify",
-    component: VuetifyView,
-  },
   {
     path: "/login",
     name: "login",
@@ -21,18 +16,14 @@ const routes = [
     },
   },
   {
-    path: "/home",
+    path: "/",
     name: "home",
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/template",
+    name: "template",
+    component: TemplateView,
   },
 ];
 
