98b1c6cb002b4b845d6152f2ca1b329319103687
coco <kokotenrico@gmail.com>
2022-06-19 19:00:44 +0200

    Upgrade template

--- src/views/TemplateView.vue
+++ src/views/TemplateView.vue
@@ -1,17 +1,22 @@
 <template>
   <v-container fill-height justify-center>
     <h1>This is a template page</h1>
+    <v-btn x-large color="primary" @click="goBack()">BACK</v-btn>
   </v-container>
 </template>
 
 <script>
+import store from "@/store";
+
 export default {
   name: "HomeView",
   data: () => ({
     //
   }),
   methods: {
-    //
+    goBack() {
+      this.$router.back();
+    },
   },
   mounted() {
     //
