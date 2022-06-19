851b61105dbe9a8787d02d0ee4f0202b6185aeb8
coco <kokotenrico@gmail.com>
2022-06-19 08:21:57 +0200

    Remove redundant comments

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,6 @@
   <v-app>
     <v-app-bar v-if="!$route.meta.hideNavbar" app class="indigo" dark>
       <div class="d-flex align-center">
-        <!-- <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        /> -->
         <router-link
           style="text-decoration: none; color: inherit"
           to="/vuetify"
