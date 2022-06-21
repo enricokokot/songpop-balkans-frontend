05240ee4399ccadb2146a69ec39a2c939a9c4092
coco <kokotenrico@gmail.com>
2022-06-21 16:11:38 +0200

    Improve play screen ui

--- src/views/PlayView.vue
+++ src/views/PlayView.vue
@@ -1,37 +1,84 @@
 <template>
-  <v-container justify-center>
-    <h1>Choose a person to duel against</h1>
-    <v-container v-for="player in players" :key="player.id">
-      <v-card elevation="2" outlined>
-        <v-card-title>{{ player.name }}</v-card-title>
-        <v-card-subtitle v-if="player.result[0] !== 0 && player.result[1] !== 0"
-          >{{ player.result[0] }} - {{ player.result[1] }}</v-card-subtitle
-        >
-        <v-card-text></v-card-text>
-        <v-card-actions
-          ><v-btn
-            v-if="player.status === 'waiting'"
-            x-large
-            color="primary"
-            @click="playADuel(player)"
-            disabled
-            >{{ player.status }}</v-btn
-          ><v-btn v-else x-large color="primary" @click="playADuel(player)">{{
-            player.status
-          }}</v-btn
-          ><v-btn
-            v-if="player.status !== 'challenge' && player.status !== 'waiting'"
-            x-large
-            color="error"
-            @click="quitADuel(player.id)"
-            >QUIT
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-container>
-    <v-container justify-center>
-      <v-btn x-large color="primary" @click="goBack()">BACK </v-btn>
-    </v-container>
+  <v-container fill-height justify-center>
+    <v-col></v-col>
+    <v-responsive width="80%">
+      <v-col>
+        <h1 class="text-center">Who do you want to play against?</h1>
+        <v-container v-for="player in players" :key="player.id">
+          <v-card elevation="2" outlined>
+            <v-row class="ma-1">
+              <v-col>
+                <v-avatar color="primary" size="10vh">
+                  <span class="white--text text-h5">{{
+                    player.name
+                      .split(" ")
+                      .map((word) => word[0])
+                      .join("")
+                  }}</span>
+                </v-avatar>
+              </v-col>
+              <v-col
+                ><v-container fill-height>
+                  <v-row>{{ player.name }}</v-row>
+                  <v-row v-if="player.result[0] !== 0 && player.result[1] !== 0"
+                    >Yesterday</v-row
+                  >
+                </v-container>
+              </v-col>
+              <v-col
+                ><v-container
+                  fill-height
+                  v-if="player.result[0] !== 0 && player.result[1] !== 0"
+                >
+                  <div class="text-h3">
+                    {{ player.result[0] }} - {{ player.result[1] }}
+                  </div>
+                </v-container></v-col
+              >
+              <v-col
+                ><v-container fill-height>
+                  <v-btn
+                    v-if="player.status === 'waiting'"
+                    x-large
+                    color="primary"
+                    @click="playADuel(player)"
+                    disabled
+                    block
+                    text
+                    >Their Turn</v-btn
+                  >
+                  <v-btn
+                    v-else
+                    x-large
+                    color="primary"
+                    @click="playADuel(player)"
+                    block
+                    text
+                    >{{ player.status }}</v-btn
+                  >
+                  <!-- <v-btn
+                    v-if="
+                      player.status !== 'challenge' &&
+                      player.status !== 'waiting'
+                    "
+                    x-large
+                    color="error"
+                    @click="quitADuel(player.id)"
+                    block
+                    text
+                    >QUIT
+                  </v-btn> -->
+                </v-container>
+              </v-col>
+            </v-row>
+          </v-card>
+        </v-container>
+        <v-container fill-height justify-center>
+          <v-btn x-large color="primary" @click="goBack()" block>BACK </v-btn>
+        </v-container>
+      </v-col>
+    </v-responsive>
+    <v-col></v-col>
   </v-container>
 </template>
 
