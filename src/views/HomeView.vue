f3c550acfea9a619f0a57e3eb56e1a6b9816994f
coco <kokotenrico@gmail.com>
2022-06-21 12:51:27 +0200

    Improve home screen ui

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,15 @@
 <template>
   <v-container fill-height justify-center>
-    <v-btn x-large color="primary" @click="goToPlay()">PLAY</v-btn>
-    <v-btn x-large color="primary" @click="goToStore()">STORE</v-btn>
+    <v-col></v-col>
+    <v-col>
+      <v-btn x-large color="primary" class="pa-0 ma-2" @click="goToPlay()"
+        >PLAY</v-btn
+      >
+      <v-btn x-large color="primary" class="pa-0 ma-2" @click="goToStore()"
+        >STORE</v-btn
+      >
+    </v-col>
+    <v-col></v-col>
   </v-container>
 </template>
 
