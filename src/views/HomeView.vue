0bf4361bf5af87a957aa7e8df23555bdbfee4fae
coco <kokotenrico@gmail.com>
2022-06-23 14:51:23 +0200

    Fix alignment

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,31 +1,37 @@
 <template>
   <v-container fill-height>
-    <v-row class="align-center justify-center">
-      <v-btn
-        x-large
-        color="primary"
-        class="ma-2"
-        @click="goToPlay()"
-        width="15%"
-        >PLAY</v-btn
-      >
-      <v-btn
-        x-large
-        color="primary"
-        class="ma-2"
-        @click="goToAchievements()"
-        width="15%"
-        >ACHIEVEMENTS</v-btn
-      >
-      <v-btn
-        x-large
-        color="primary"
-        class="ma-2"
-        @click="goToStore()"
-        width="15%"
-        >STORE</v-btn
-      >
-    </v-row>
+    <v-col>
+      <v-row class="align-center justify-center">
+        <v-btn
+          x-large
+          color="primary"
+          class="ma-2"
+          @click="goToPlay()"
+          width="15%"
+          >PLAY</v-btn
+        >
+      </v-row>
+      <v-row class="align-center justify-center">
+        <v-btn
+          x-large
+          color="primary"
+          class="ma-2"
+          @click="goToAchievements()"
+          width="15%"
+          >ACHIEVEMENTS</v-btn
+        >
+      </v-row>
+      <v-row class="align-center justify-center">
+        <v-btn
+          x-large
+          color="primary"
+          class="ma-2"
+          @click="goToStore()"
+          width="15%"
+          >STORE</v-btn
+        >
+      </v-row>
+    </v-col>
   </v-container>
 </template>
 
