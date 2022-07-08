3a8aa06a31166f63bef0bb310c9697b362397338
coco <kokotenrico@gmail.com>
2022-07-08 09:51:51 +0200

    Introduce animated route transition

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,11 @@
     </v-app-bar>
 
     <v-main class="indigo lighten-5">
-      <router-view />
+      <transition name="slide" mode="out-in">
+        <!-- <transition name="moveUp"> -->
+        <!-- <transition name="fade"> -->
+        <router-view />
+      </transition>
     </v-main>
   </v-app>
 </template>
@@ -42,3 +46,51 @@ export default {
   },
 };
 </script>
+
+<style>
+/* slide */
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity 0.1s, transform 0.1s;
+}
+.slide-enter,
+slide-leave-to {
+  opacity: 0;
+  transform: translateX(-30%);
+}
+/* moveUp */
+.moveUp-enter-active {
+  animation: fadeIn 1s ease-in;
+}
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  50% {
+    opacity: 0.5;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+.moveUp-leave-active {
+  animation: moveUp 0.3s ease-in;
+}
+@keyframes moveUp {
+  0% {
+    transform: translateY(0);
+  }
+  100% {
+    transform: translateY(-400px);
+  }
+}
+/* fade */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
