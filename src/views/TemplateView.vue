f7e24775f87ebcacc8232428f31e4e0494c0f11b
coco <kokotenrico@gmail.com>
2022-07-09 10:01:02 +0200

    Add grid system example

--- src/views/TemplateView.vue
+++ src/views/TemplateView.vue
@@ -1,8 +1,32 @@
 <template>
-  <v-container fill-height justify-center>
-    <h1>This is a template page</h1>
-    <v-btn x-large color="primary" @click="goBack()">BACK</v-btn>
-  </v-container>
+  <div class="dashboard">
+    <h1 class="subheading grey--text">Dashboards</h1>
+    <v-container class="">
+      <v-layout row wrap>
+        <v-flex xs12 md6>
+          <v-btn outline block class="primary">1</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class="primary">2</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class="primary">2</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class="primary">2</v-btn>
+        </v-flex>
+      </v-layout>
+
+      <v-layout row wrap justify-space-around>
+        <v-flex xs4 md3>
+          <v-btn outline block class="success">1</v-btn>
+        </v-flex>
+        <v-flex xs4 md3>
+          <v-btn outline block class="success">2</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
