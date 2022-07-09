df1077d54fd666c91803e20220da3e620997668c
coco <kokotenrico@gmail.com>
2022-07-09 15:11:59 +0200

    Improve ui of login

--- src/components/SigninForm.vue
+++ src/components/SigninForm.vue
@@ -1,71 +1,41 @@
 <template>
-  <div class="about d-flex flex-column pa-16">
-    <h1 class="text-center">Signin</h1>
+  <div class="about d-flex flex-column px-16">
+    <h1 class="text-center">Sign In</h1>
     <v-form ref="form" v-model="valid" lazy-validation>
       <v-text-field
-        v-model="email"
-        :rules="[rules.required]"
-        label="E-mail"
+        v-model="username"
+        :rules="nameRules"
+        label="username"
         required
       ></v-text-field>
 
       <v-text-field
         v-model="password"
         :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-        :rules="[rules.required, rules.min]"
         :type="show ? 'text' : 'password'"
-        label="Password"
-        hint="At least 8 characters"
-        counter
+        :rules="passwordRules"
         @click:append="show = !show"
+        label="password"
+        required
       ></v-text-field>
 
       <v-select
         v-model="select"
         :items="genres"
-        :rules="[rules.required]"
-        label="Preferred Genre"
+        :rules="playlistRules"
+        label="preferred playlist"
         required
       ></v-select>
 
-      <v-container class="d-flex flex-column justify-start">
-        <!-- <v-btn
-          :disabled="!valid"
-          color="transparent"
-          class="success--text ma-1"
-          :elevation="0"
-          @click="validate"
-        >
-          Sign in
-        </v-btn> -->
-        <v-btn
-          :disabled="!valid"
-          color="transparent"
-          class="success--text ma-1"
-          :elevation="0"
-          @click="register"
-        >
-          Sign in
-        </v-btn>
-
-        <v-btn
-          color="transparent"
-          class="error--text ma-1"
-          :elevation="0"
-          @click="reset"
-        >
-          Reset Form
-        </v-btn>
-
-        <v-btn
-          color="transparent"
-          class="warning--text ma-1"
-          :elevation="0"
-          @click="resetValidation"
-        >
-          Reset Validation
-        </v-btn>
-      </v-container>
+      <v-btn
+        :disabled="!valid"
+        color="primary"
+        @click="validate"
+        class="my-5"
+        block
+      >
+        SIGN IN
+      </v-btn>
     </v-form>
   </div>
 </template>
@@ -77,46 +47,32 @@ export default {
   data: () => ({
     show: false,
     valid: true,
-    email: "",
-    emailRules: [
-      (v) => !!v || "E-mail is required",
-      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+    username: "",
+    nameRules: [
+      (v) => !!v || "username is required",
+      (v) => v.indexOf(" ") < 0 || "username can't contain whitespaces",
     ],
     password: "",
-    passwordRules: [
-      (v) => !!v || "Password is required",
-      (v) =>
-        (v && v.length >= 8) || "Password must be at least 8 characters long",
-    ],
+    passwordRules: [(v) => !!v || "password is required"],
     select: null,
+    playlistRules: [(v) => !!v || "playlist is required"],
     genres: ["Rock", "Folk"],
-    rules: {
-      required: (value) => !!value || "Required.",
-      min: (v) => v.length >= 8 || "Min 8 characters",
-      emailMatch: () => `The email and password you entered don't match`,
-    },
   }),
 
   methods: {
+    validate() {
+      this.$refs.form.validate() && this.register("/home");
+    },
     async register() {
       const data = {
-        username: this.email,
+        username: this.username,
         password: this.password,
         playlist: this.select,
       };
       const registered = await Users.register(data);
       console.log("registered:", registered);
-      const success = await Auth.login(this.email, this.password);
+      const success = await Auth.login(this.username, this.password);
       success && this.$router.push("/home");
-    },
-    // validate() {
-    //   if (this.$refs.form.validate()) this.$router.push("/about");
-    // },
-    reset() {
-      this.$refs.form.reset();
-    },
-    resetValidation() {
-      this.$refs.form.resetValidation();
     },
   },
 };
