b476904650780d05afe665dadbf5087de841ba55
coco <kokotenrico@gmail.com>
2022-07-14 16:08:48 +0200

    Minor style and function improvement

--- src/views/AchievementsView.vue
+++ src/views/AchievementsView.vue
@@ -5,24 +5,29 @@
         <v-row class="align-center justify-center pa-2"
           ><h1>This is your achievement progress so far</h1></v-row
         >
-        <v-container v-for="achievement in achievements" :key="achievement.id">
-          <v-card
-            elevation="2"
-            outlined
-            :disabled="[1, 2].includes(achievement.id)"
+        <v-scroll-x-transition group>
+          <v-container
+            v-for="achievement in achievements"
+            :key="achievement.id"
           >
-            <v-row class="ma-2">{{ achievement.mission }}</v-row>
-            <v-spacer></v-spacer>
-            <v-row class="ma-2"
-              >{{ achievement.progress }}/{{ achievement.total }}</v-row
+            <v-card
+              elevation="2"
+              outlined
+              :disabled="[1, 2].includes(achievement.id)"
             >
-            <v-row class="ma-2">
-              <v-progress-linear
-                :value="(achievement.progress / achievement.total) * 100"
-              ></v-progress-linear>
-            </v-row>
-          </v-card>
-        </v-container>
+              <v-row class="ma-2">{{ achievement.mission }}</v-row>
+              <v-spacer></v-spacer>
+              <v-row class="ma-2"
+                >{{ achievement.progress }}/{{ achievement.total }}</v-row
+              >
+              <v-row class="ma-2">
+                <v-progress-linear
+                  :value="(achievement.progress / achievement.total) * 100"
+                ></v-progress-linear>
+              </v-row>
+            </v-card>
+          </v-container>
+        </v-scroll-x-transition>
         <v-row class="align-center justify-center pa-2 mt-8">
           <v-btn x-large color="primary" @click="goBack()" width="30%"
             >BACK
@@ -34,23 +39,25 @@
 </template>
 
 <script>
-import store from "@/store";
+import { Auth, Users } from "@/services";
 
 export default {
   name: "AchievementsView",
   data: () => ({
+    userId: Auth.state.user.userId,
     achievements: [],
   }),
   methods: {
     goBack() {
       this.$router.back();
     },
-    fetchData() {
-      this.achievements = store.achievements;
+    async fetchAllAchievements() {
+      const allAchievements = await Users.getAllAchievements(this.userId);
+      return allAchievements;
     },
   },
-  mounted() {
-    this.fetchData();
+  async mounted() {
+    this.achievements = await this.fetchAllAchievements();
   },
   computed: {
     //
